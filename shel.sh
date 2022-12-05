cbc07cb34062afb03346349baaae8aef445689f0
Gil Eduardo <119633156+ge0ti@users.noreply.github.com>
2022-12-05 15:46:08 -0300

    Add files via upload
    shell script code to read memory and interface processor

---
+++ shel.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+  clear
+  echo "status host !"
+  #sleep(1)
+  whoami
+  hostname
+ echo "status memory.."
+    free -g
+ echo "status kernel version & Processor !"
+  uname -r    # kernel version 
+  uname -o    # S.O  version 
+echo " status Processor Version !" 
+   uname -v    # kernel version " 
+  uname -m    # Arquit version " 
